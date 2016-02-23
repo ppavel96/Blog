8376d4907de1fa57941d43bab1446b34c79199c9
Polyakov Pavel <papolyakov_1@edu.hse.ru>
2016-02-23 23:47:29 +0300

    Redirects

--- blog/urls.py
+++ blog/urls.py
@@ -1,18 +1,20 @@
 ﻿from django.conf.urls import url
 from django.views.generic import TemplateView
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.posts, name='posts'),
-    url(r'^posts/?$', views.posts, name='posts'),
-    url(r'^posts/(hot|new|best|feed)$', views.posts, name='posts'),
+    url(r'^$', RedirectView.as_view(url='/posts/hot/', permanent=True)),
+    url(r'^posts/$', RedirectView.as_view(url='/posts/hot/', permanent=True)),
 
-    url(r'^blogs$', views.blogs, name='blogs'),
-    url(r'^people$', views.people, name='people'),
-    url(r'^about$', views.about, name='about'),
+    url(r'^posts/(hot|new|best|feed)/$', views.posts, name='posts'),
+
+    url(r'^blogs/$', views.blogs, name='blogs'),
+    url(r'^people/$', views.people, name='people'),
+    url(r'^about/$', views.about, name='about'),
 
     url(r'^robots.txt$', TemplateView.as_view(template_name='blog/robots.txt', content_type='text/plain')),
     url(r'^humans.txt$', TemplateView.as_view(template_name='blog/humans.txt', content_type='text/plain')),
 
-    url(r'^(?!admin$).*', views.page_404, name='page_404'),
+    url(r'^(?!admin$).*/$', views.page_404, name='page_404'),
 ]
