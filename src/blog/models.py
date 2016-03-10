baa032a85f71be2cf232d8df88ba372afff7c723
Polyakov Pavel <papolyakov_1@edu.hse.ru>
2016-03-10 22:45:55 +0300

    Updating post publish time

--- src/blog/models.py
+++ src/blog/models.py
@@ -17,12 +17,23 @@ class Post(models.Model):
                  'blog' : self.blog,
                  'title' : self.title,
                  'HTMLContent' : self.HTMLContent,
-                 'publishedDate' : self.publishedDate,
+                 'publishedDate' : self.publishedDate.isoformat(),
                  'rating' : self.rating }
 
     def publish(self):
         self.publishedDate = timezone.now()
         self.save()
+
+    def __str__(self):
+        return self.title
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    avatar = models.CharField(max_length=200)
+
+    created_date = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return self.title
--- src/blog/static/js/loadData.js
+++ src/blog/static/js/loadData.js
@@ -17,6 +17,16 @@
     }
 }
 
+timersToUpdate = [];
+function timersUpdate() {
+    for (var i = 0; i < timersToUpdate.length; i++) {
+        var mins = Math.ceil((Date.now() - timersToUpdate[i].time) / 60000);
+        $('#' + timersToUpdate[i].id).text(mins.toString());
+    }
+}
+
+setInterval(timersUpdate, 60000);
+
 function loadPosts(inCategory) {
     if (loadPosts.nextID == undefined) {
         loadPosts.nextID = 0;
@@ -25,15 +35,19 @@ function loadPosts(inCategory) {
 
     var LOAD_COUNT = 5;
 
-    request = { navigation: 'posts', category: inCategory, id: loadPosts.nextID, count: LOAD_COUNT };
-    callback = function (posts) {
+    var request = { navigation: 'posts', category: inCategory, id: loadPosts.nextID, count: LOAD_COUNT };
+    var callback = function (posts) {
         for (var i = 0; i < posts.length; i++) {
             var postClass = (i == 0 && loadPosts.nextID == 0) ? 'post' : 'post indent-needed';
+            timersToUpdate.push({
+                id: "publishTime_" + (loadPosts.nextID + i).toString(),
+                time: Date.parse(posts[i].publishedDate)
+            });
 
             var text = '<div class="' + postClass + '">' +
                        '    <div class="content-inner">' +
                        '        <h3>' + posts[i].title + '</h3>' +
-                       '        <p class="tiny">Опубликовал ' + posts[i].author + ' в ' + posts[i].blog + ' n минут назад</p>' +
+                       '        <p class="tiny">Опубликовал ' + posts[i].author + ' в ' + posts[i].blog + ' <span id="' + timersToUpdate[timersToUpdate.length - 1].id + '"></span>' + ' минут назад</p>' +
                        '        <br />';
 
             text += posts[i].HTMLContent;
@@ -48,9 +62,10 @@ function loadPosts(inCategory) {
         loadPosts.nextID += posts.length;
         if (posts.length < LOAD_COUNT)
             loadPosts.noMore = true;
+
+        timersUpdate();
     }
 
     if (!loadPosts.noMore)
         loadData(request, callback);
 }
- 
